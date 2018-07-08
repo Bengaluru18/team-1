a2dea7d79baa99ab766b60746f2122106f6bb00a
abhijithkrishnan0 <36269802+abhijithkrishnan0@users.noreply.github.com>
2018-07-08 09:00:05 +0530

    commit

--- CWF/app/src/main/java/com/cwf/jpmorgan/cwf/QuestionActivity.java
+++ CWF/app/src/main/java/com/cwf/jpmorgan/cwf/QuestionActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,14 +25,23 @@ public class QuestionActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_question);
        // ListView listView1;
-        
-
-         EditText Ans1=(EditText)findViewById(R.id.ans1);
-        EditText Ans2=(EditText)findViewById(R.id.ans2);
+        final int qid=1;
+        EditText Ans1=(EditText)findViewById(R.id.ans1);
+        EditText Ans2= (EditText)findViewById(R.id.ans2);
         EditText Ans3=(EditText)findViewById(R.id.ans3);
         EditText Ans4=(EditText)findViewById(R.id.ans4);
-        EditText Ans5=(EditText)findViewById(R.id.ans5);
+        EditText Ans5= (EditText)findViewById(R.id.ans5);
 
+        Log.i("abc","abcd");
+
+
+
+        Ans1=(EditText)findViewById(R.id.ans1);
+        Ans2=(EditText)findViewById(R.id.ans2);
+        Ans3=(EditText)findViewById(R.id.ans3);
+        Ans4=(EditText)findViewById(R.id.ans4);
+        Ans5=(EditText)findViewById(R.id.ans5);
+        Log.i("abc","abcd1");
         Button submit=(Button)findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,13 +57,23 @@ public class QuestionActivity extends AppCompatActivity{
                  //   mDatabase.child(userId.replace(".",",")).child(mDatabase.push().getKey()).setValue(m1);
                 String id=mDatabase.push().getKey();
                 Log.i("abc",id);
-
+                Survey m1=new Survey();
                 DatabaseReference mDatabase=FirebaseDatabase.getInstance().getReference("Surveyss");
 
-                Survey m1=new Survey(Ans1.getText() .toString(),"1","abc");
+                    m1.Surveyenter(Ans1.getText().toString(), String.valueOf(qid), Ans2.getText().toString());
+                    mDatabase.child(mDatabase.push().getKey()).setValue(m1);
+                m1.Surveyenter(Ans1.getText().toString(), String.valueOf(qid), Ans3.getText().toString());
                 mDatabase.child(mDatabase.push().getKey()).setValue(m1);
-                Log.i("ejfnaw","afkjan");
-
+                m1.Surveyenter(Ans1.getText().toString(), String.valueOf(qid), Ans4.getText().toString());
+                mDatabase.child(mDatabase.push().getKey()).setValue(m1);
+                m1.Surveyenter(Ans1.getText().toString(), String.valueOf(qid), Ans5.getText().toString());
+                mDatabase.child(mDatabase.push().getKey()).setValue(m1);
+                Ans1.setText("");
+                Ans2.setText("");
+                Ans3.setText("");
+                Ans4.setText("");
+                Ans5.setText("");
+                Toast.makeText(QuestionActivity.this,"DATA UPLOADED",Toast.LENGTH_LONG).show();
             }
         });
 
